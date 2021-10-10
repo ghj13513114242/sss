d8b24f8d34d708b723f8b400acf26f8492e3c793
ghj <2260038926>
2021-10-10 14:38:20 +0800

    ss

---
+++ demo/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component, Suspense, lazy } from "react";
+import { withRouter, NavLink, Switch, Redirect, Route } from "react-router-dom";
+import Home from './views/Home'
+
+const Home = lazy(() => import('./routes/Home'));
+class App extends Component {
+  render() {
+    return (
+      <Suspense fallback={<div>Loading..................................</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+
+        </Switch>
+      </Suspense>
+    );
+  }
+}
+
+export default App;
